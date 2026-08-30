c7aeaec9abc45905639cf09bfc9105958b646356
Abdulfattah Alturkistanigit config --global user.email abdulfatah.h.turkstni@gmail.com <abdulfatah.h.turkstni@gmail.com>
2026-08-31 01:30:02 +0300

    Rename file

--- Labs/Lab_2.java
+++ Labs/Lab_2.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Lab {
+public class Lab_2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         // Fix decimal format error (use dot '.' instead of comma)
